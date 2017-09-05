961ed57232b6f7a9aefdaf8b7c87455804c02180
coderguanmingyang <1656499080@qq.com>
2017-09-05 18:49:14 +0800

    orignal

---
+++ cifar/resource/testImage.lua
@@ -0,0 +1,11 @@
+--
+-- Created by IntelliJ IDEA.
+-- User: PC
+-- Date: 2017/9/4
+-- Time: 22:17
+-- To change this template use File | Settings | File Templates.
+--
+require 'torch'
+
+local image = torch.load('genImage.t7')
+print(image)
